e522efbbd807374262ac594dd4cc4f209def5523
Fox Say <Lisick1999@yandex.ru>
2024-02-01 21:47:24 +0300

    Оба задания сделаны полностью

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,23 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
-  let arr = [];
+  const arr = [];
+  const d = b**2 - 4 * a * c;
+  if (d < 0) {
+    return arr;
+  } else if (d === 0) {
+    arr[0] = -b / (2 * a);
+  } else {
+    arr[0] = (-b + Math.sqrt(d)) / (2 * a);
+    arr[1] = (-b - Math.sqrt(d)) / (2 * a);
+  }
   
   return arr;
 }
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+function calculateTotalMortgage(percent, contribution, amount, countMonths) { 
+  const monthlyRate = percent / (100 * 12);
+  const payment = (amount - contribution) * (monthlyRate + (monthlyRate / (((1 + monthlyRate)**countMonths) - 1)));
+  const totalAmount = (payment * countMonths).toFixed(2);
+
+  return Number(totalAmount);
 }
